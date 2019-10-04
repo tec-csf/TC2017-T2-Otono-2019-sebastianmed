f3136f5431817227302cc41dd11310b2f892b686
Sebastián Medina <31284122+sebastianmed@users.noreply.github.com>
2019-10-04 13:32:36 -0500

    Update main.cpp

--- Tarea3/Camionero/main.cpp
+++ Tarea3/Camionero/main.cpp
@@ -2,6 +2,8 @@
  
  PROBLEMA:
  Un camionero conduce desde DF a Acapulco siguiendo una ruta dada y llevando un camión que le permite, con el tanque de gasolina lleno, recorrer n kilómetros sin parar. El camionero dispone de un mapa de carreteras que le indica las distancias entre las gasolineras que hay en su ruta. Como va con prisa, el camionero desea detenerse a abastecer gasolina el menor número de veces posible.
+
+ Es un algoritmo ávido.
  
  Complejidad: O(n) debido a que el problema solo tiene un bucle para contar el número de estaciones de gas
 */
